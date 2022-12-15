41d860c08c05c8ceb8126fa8f309758e214a32bd
Jinwon Seo <jwseo@live.co.kr>
2022-12-15 17:07:55 +0900

    Initialize log 변경 & Bean to Static class. Closed #4

--- src/main/java/kr/sprouts/autoconfigure/configurations/JwtParserConfiguration.java
+++ src/main/java/kr/sprouts/autoconfigure/configurations/JwtParserConfiguration.java
@@ -1,21 +1,13 @@
 package kr.sprouts.autoconfigure.configurations;
 
-import kr.sprouts.autoconfigure.components.JwtParser;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JwtParserConfiguration {
-    private final Logger logger = LoggerFactory.getLogger(JwtParserConfiguration.class);
 
     public JwtParserConfiguration() {
-        this.logger.info(String.format("Initialized %s", JwtParserConfiguration.class.getName()));
-    }
-
-    @Bean
-    public JwtParser jwtParser() {
-        return new JwtParser();
+        LoggerFactory.getLogger(JwtParserConfiguration.class)
+                .info(String.format("Initialized %s", JwtParserConfiguration.class.getName()));
     }
 }
--- src/test/java/kr/sprouts/autoconfigure/configurations/JwtParserConfigurationTest.java
+++ src/test/java/kr/sprouts/autoconfigure/configurations/JwtParserConfigurationTest.java
@@ -1,26 +1,22 @@
 package kr.sprouts.autoconfigure.configurations;
 
-import kr.sprouts.autoconfigure.components.JwtHelper;
-import kr.sprouts.autoconfigure.components.JwtParser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class JwtIssuerParserConfigurationTest {
-    private final ApplicationContextRunner applicationContextRunner = new ApplicationContextRunner()
-            .withConfiguration(AutoConfigurations.of(JwtHelperConfiguration.class))
-            .withConfiguration(AutoConfigurations.of(JwtParserConfiguration.class));
+class JwtParserConfigurationTest {
+    private final ApplicationContextRunner applicationContextRunner= new ApplicationContextRunner().withConfiguration(
+            AutoConfigurations.of(JwtParserConfiguration.class)
+    );
 
     @Test
     public void bean_test() {
         this.applicationContextRunner.run(
                 context -> {
                     assertThat(context).hasSingleBean(JwtHelperConfiguration.class);
-                    assertThat(context).hasSingleBean(JwtHelper.class);
                     assertThat(context).hasSingleBean(JwtParserConfiguration.class);
-                    assertThat(context).hasSingleBean(JwtParser.class);
                 }
         );
     }
